ae4108b60b37c8ad4c23e3eb8d75a0267233ca44
ubunknown1 <ubunknown1@protonmail.com>
2022-12-25 07:39:31 +0100

    initial commit

--- config.sh
+++ config.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ 
 
+ 
 
-packages=$"virt-what nautilus-admin software-properties-common build-essential gnupg gnupg1 gnupg2 kgpg git gh ubuntu-restricted-extras openssh-client openssh-server ufw guake tor python3.10-full snap snapd timeshift gnome-tweaks gcc g++ openjdk-18-jre-headless gedit macchanger iw bleachbit python3.10-full"
+packages=$"virt-what nautilus-admin plymouth-themes software-properties-common build-essential gnupg gnupg1 gnupg2 kgpg git gh ubuntu-restricted-extras openssh-client openssh-server ufw guake tor python3.10-full snap snapd timeshift gnome-tweaks gcc g++ openjdk-18-jre-headless gedit macchanger iw bleachbit python3.10-full"
 update() {
 	echo "System update"
 	sudo apt update &> /dev/null
@@ -27,10 +29,25 @@ configure_system() {
 	git config --global user.email ubunknown1@protonmail.com
 	git config --global user.name ubunknown1
 	
+	# setup GPG
+	dir=$(pwd)
+	echo "$dir/ssh-"
+	gpg --import $dir/public.gpg
+	gpg --import $dir/private.gpg
+	gpg --import-ownertrust $dir/trust.gpg
+	gpg --list-secret-keys --keyid-format LONG
+$ 	chmod 600 ~/.ssh/id_rsa
+$	chmod 644 ~/.ssh/id_rsa.pub
+	
+	# setup SSH
+	
+	
 	# enable ssh firewall
 	sudo ufw allow ssh
 	sudo systemctl restart ssh
 	sudo systemctl enable ssh
 }
 
+update
+installer
 configure_system
---
+++ ssh-config.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+dir=$(pwd)
+echo "$dir/ssh-"
+#gpg --import public.gpg
+#gpg --import private.gpg
+#gpg --import-ownertrust trust.gpg
+#gpg --list-secret-keys --keyid-format LONG
